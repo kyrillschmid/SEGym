0be4f23c08282e649451b278ec9a7f516cca183d
Gerhard Stenzel <G.Stenzel@campus.lmu.de>
2024-05-11 00:41:55 +0200

    refactor executor

--- se_gym/executor.py
+++ se_gym/executor.py
@@ -16,6 +16,8 @@ DOCKER_TAG = "pytest-env"
 
 logger = logging.getLogger("docker_connector")
 
+GIT_APPLY_PATCH = "git apply --ignore-space-change --ignore-whitespace --verbose --recount --inaccurate-eof ./file.patch"
+
 
 class DockerConnector:
     def __init__(self):
@@ -52,6 +54,33 @@ class MalformedPatchException(Exception):
     pass
 
 
+class Container:
+    def __init__(self, mount_dir: str):
+        self.mount_dir = mount_dir
+        self.container = DockerConnector.get_instance().client.containers.run(
+            image=DOCKER_TAG,
+            detach=True,
+            volumes={mount_dir: {"bind": "/repo", "mode": "rw"}},
+            working_dir="/repo",
+            tty=True,
+            name=f"se_gym_container_{time.time()}",
+        )
+
+    def run_command(self, command: str):
+        return self.container.exec_run(cmd=command, stdout=True, stderr=True)
+
+    def stop(self):
+        self.container.stop()
+        self.container.remove(
+            v={self.mount_dir: {"bind": "/repo", "mode": "rw"}}, force=True
+        )
+
+
+def _shutil_onexc(func, path, exc_info):
+    os.chmod(path, stat.S_IWRITE)
+    os.unlink(path)
+
+
 def apply_patch(code_base_root: str, patch: str):
     """
     Apply a patch to a codebase.
@@ -64,61 +93,23 @@ def apply_patch(code_base_root: str, patch: str):
     temp_dir = tempfile.mkdtemp(prefix="se_gym_")
     logger.debug(f"Created temporary directory {temp_dir}")
     # Copy the codebase to the temporary directory
-    shutil.copytree(
-        src=code_base_root,
-        dst=f"{temp_dir}/",
-        dirs_exist_ok=True,
-    )
-    # Write the patch to a file
+    shutil.copytree(src=code_base_root, dst=f"{temp_dir}/", dirs_exist_ok=True)
+
     with open(f"{temp_dir}/file.patch", "w") as file:
-        file.write(patch.strip())
-        file.write("\n")
-    # Start a container with the codebase
-    container = DockerConnector.get_instance().client.containers.run(
-        DOCKER_TAG,
-        detach=True,
-        volumes={temp_dir: {"bind": "/repo", "mode": "rw"}},
-        working_dir="/repo",
-        # command="/bin/bash",
-        tty=True,
-        name=f"se_gym_container_{time.time()}",
-    )
-    # container.wait()
-    # Attempt to apply the patch in the container
-    apply_log = container.exec_run(
-        cmd=[
-            "git",
-            "apply",
-            "--ignore-space-change",
-            "--ignore-whitespace",
-            "--verbose",
-            "--recount",
-            "--inaccurate-eof",
-            "./file.patch",
-        ],
-        stdout=True,
-        stderr=True,
-    )
-    print(apply_log)
+        file.write(patch)  # Write the patch to a file
+
+    container = Container(mount_dir=temp_dir)  # Start a container with the codebase
+    apply_log = container.run_command(GIT_APPLY_PATCH)  # Try to patch
 
     # Tear down the container
-    # container.wait()
     container.stop()
-    container.remove(v={temp_dir: {"bind": "/repo", "mode": "rw"}}, force=True)
+
     # remove the temporary directory
-
-    def on_rm_error(func, path, exc_info):
-        os.chmod(path, stat.S_IWRITE)
-        os.unlink(path)
-
-    shutil.rmtree(temp_dir, onexc=on_rm_error)
+    shutil.rmtree(temp_dir, onexc=_shutil_onexc)
 
     # Check if the patch was applied successfully
     if apply_log.exit_code != 0:
-        logger.info("Failed to apply patch")
-        logger.error(apply_log.output)
-        raise MalformedPatchException(
-            "Failed to apply patch", apply_log.output.decode("utf-8")
-        )
+        outp = apply_log.output.decode("utf-8")
+        logger.info("Failed to apply patch", outp)
+        raise MalformedPatchException("Failed to apply patch", outp)
     logger.info("Patch applied successfully")
-    print("success")
